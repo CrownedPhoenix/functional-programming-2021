a68e1b5a8e2b093d43b8a0bccaae340e7287d1f5
Jairon Terrero <jairon.s.terrero@gmail.com>
2021-02-22 22:07:54 -0700

    Fixed escript?

--- santorini/lib/santorini/cli.ex
+++ santorini/lib/santorini/cli.ex
@@ -1,5 +1,6 @@
 defmodule Santorini.CLI do
   import OptionParser
+  import Santorini.CLIFuncs
   alias Santorini.Board, as: Board
   alias Santorini.BoardUtils, as: BoardUtils
 
@@ -63,41 +64,6 @@ defmodule Santorini.CLI do
       _ ->
         play()
     end
-  end
-
-  def play() do
-    players = IO.read(:stdio, :line) |> Jason.decode!()
-
-    b =
-      case length(players) do
-        0 ->
-          [[3, 3], [1, 3]] |> Jason.encode!() |> IO.puts()
-
-          IO.read(:stdio, :line) |> BoardUtils.from_json()
-
-        1 ->
-          other = Enum.at(players, 0)
-
-          mine =
-            Stream.repeatedly(fn -> [:rand.uniform(5) - 1, :rand.uniform(5) - 1] end)
-            |> Stream.filter(fn pos -> pos not in other end)
-            |> Stream.take(2)
-            |> Enum.to_list()
-
-          Board.new() |> Board.update_players(players ++ [mine]) |> Board.update_turn(3)
-      end
-
-    Stream.unfold(b, fn b ->
-      # TODO: Determine optimal actionId
-      actionId = 0
-
-      BoardUtils.action(b, actionId)
-      |> BoardUtils.to_json()
-      |> IO.puts()
-
-      {b, BoardUtils.from_json(IO.read(:stdio, :line))}
-    end)
-    |> Stream.run()
   end
 end
 
