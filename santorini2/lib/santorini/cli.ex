51fc14f8ed4a975ae1ee90b99df21a85e8050bee
Jairon Terrero <jairon.s.terrero@gmail.com>
2021-03-17 19:39:58 -0600

    Hotfixes

--- santorini2/lib/santorini/cli.ex
+++ santorini2/lib/santorini/cli.ex
@@ -65,7 +65,7 @@ defmodule Santorini.CLI do
       [turn: true] ->
         IO.read(:stdio, :line)
         |> BoardUtils.from_json()
-        |> BoardUtils.take_turn()
+        |> BoardUtils.take_turn(:random)
         |> Board.swap_players()
         |> BoardUtils.to_json()
         |> IO.puts()
@@ -95,39 +95,63 @@ defmodule Santorini.CLI do
 
   def play(strategy) do
     players = IO.read(:stdio, :line) |> Jason.decode!([{:keys, :atoms}])
+    # IO.inspect(:stderr, players, [])
 
     b =
       case players do
-        [%{card: my_card}, %{card: _, tokens: _}] ->
+        [%{card: my_card}, %{card: c2, tokens: t2}] ->
           other = Enum.at(players, 1)
 
           my_tokens =
-            Stream.repeatedly(fn -> [Enum.random(0..4), Enum.random(0..4)] end)
+            Stream.repeatedly(fn -> [Enum.random(1..5), Enum.random(1..5)] end)
             |> Stream.filter(fn pos -> pos not in other end)
             |> Stream.take(2)
             |> Enum.to_list()
 
+          # [%{card: my_card, tokens: my_tokens}, %{card: c2, tokens: t2}]
+          # |> Jason.encode!()
+          # |> IO.puts()
+
+          # both_players = IO.read(:stdio, :line) |> Jason.decode!([{:keys, :atoms}])
+
           Board.new()
           |> Board.set_players([other, %{card: my_card, tokens: my_tokens}])
+          |> BoardUtils.fix_players_list()
           |> Board.update_turn(fn _ -> 3 end)
 
         [%{card: c1}, %{card: c2}] ->
           starting_tokens =
-            Stream.repeatedly(fn -> [Enum.random(0..4), Enum.random(0..4)] end)
+            Stream.repeatedly(fn -> [Enum.random(1..5), Enum.random(1..5)] end)
             |> Stream.take(2)
             |> Enum.to_list()
 
+          # [%{card: c2}, %{card: c1, tokens: starting_tokens}]
+          # |> Jason.encode!()
+          # |> (&IO.puts(:stderr, &1)).()
+
           [%{card: c2}, %{card: c1, tokens: starting_tokens}] |> Jason.encode!() |> IO.puts()
 
-          IO.read(:stdio, :line) |> BoardUtils.from_json()
+          both_players = IO.read(:stdio, :line) |> Jason.decode!([{:keys, :atoms}])
+
+          Board.new()
+          |> Board.set_players(both_players)
+          |> BoardUtils.fix_players_list()
+          |> Board.update_turn(fn _ -> 3 end)
       end
 
     Stream.unfold(b, fn b ->
-      b
-      |> BoardUtils.take_turn(strategy)
-      # |> BoardUtils.draw(:stderr)
-      |> BoardUtils.to_json()
-      |> IO.puts()
+      # IO.puts(:stderr, "Playing\n")
+      # IO.inspect(:stderr, b, [])
+      # IO.puts(:stderr, BoardUtils.to_json(b))
+      IO.inspect(:stderr, b|> BoardUtils.to_json, [])
+      out =
+        b
+        |> BoardUtils.take_turn(strategy)
+        # |> BoardUtils.draw(:stderr)
+        |> BoardUtils.to_json()
+
+      out |> IO.puts()
+      # IO.puts(:stderr, out)
 
       {b, BoardUtils.from_json(IO.read(:stdio, :line))}
     end)
